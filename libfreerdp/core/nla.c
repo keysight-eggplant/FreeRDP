40c3ba6c25aa78858f543324eee8f80d148427e7
Johnathan Becker <john.becker@keysight.com>
2025-05-27 07:39:22 -0600

    attempt this

--- libfreerdp/core/nla.c
+++ libfreerdp/core/nla.c
@@ -55,6 +55,79 @@
 
 #define SERVER_KEY "Software\\" FREERDP_VENDOR_STRING "\\" FREERDP_PRODUCT_STRING "\\Server"
 
+#define ber_sizeof_sequence_octet_string(length) \
+	ber_sizeof_contextual_tag(ber_sizeof_octet_string(length)) + ber_sizeof_octet_string(length)
+#define ber_write_sequence_octet_string(stream, context, value, length)                \
+	ber_write_contextual_tag(stream, context, ber_sizeof_octet_string(length), TRUE) + \
+	    ber_write_octet_string(stream, value, length)
+
+SEC_WINNT_AUTH_IDENTITY* SEC_WINNT_AUTH_IDENTITY_new(char* user,
+        char* password,
+        char* domain);
+SEC_WINNT_AUTH_IDENTITY* SEC_WINNT_AUTH_IDENTITY_deepcopy(SEC_WINNT_AUTH_IDENTITY* original);
+void SEC_WINNT_AUTH_IDENTITY_free(SEC_WINNT_AUTH_IDENTITY* password_creds);
+
+csp_data_detail* csp_data_detail_new_nocopy(UINT32 KeySpec,
+        char* CardName,
+        char* ReaderName,
+        char* ContainerName,
+        char* CspName);
+csp_data_detail* csp_data_detail_new(UINT32 KeySpec,
+                                     char* cardname,
+                                     char* readername,
+                                     char* containername,
+                                     char* cspname);
+csp_data_detail* csp_data_detail_deepcopy(csp_data_detail* original);
+void csp_data_detail_free(csp_data_detail* csp);
+
+smartcard_creds* smartcard_creds_new_nocopy(char* Pin, char* UserHint, char* DomainHint,
+        csp_data_detail* csp_data);
+smartcard_creds* smartcard_creds_new(char* pin, char* userhint, char* domainhint,
+                                     csp_data_detail* cspdata);
+smartcard_creds* smartcard_creds_deepcopy(smartcard_creds* original);
+void smartcard_creds_free(smartcard_creds* that);
+
+remote_guard_package_cred* remote_guard_package_cred_new_nocopy(char* package_name,
+        unsigned credential_size,
+        BYTE*    credential);
+remote_guard_package_cred* remote_guard_package_cred_deepcopy(remote_guard_package_cred* that);
+void remote_guard_package_cred_deepfree(remote_guard_package_cred* that);
+
+remote_guard_creds* remote_guard_creds_new_logon_cred(remote_guard_package_cred* logon_cred);
+remote_guard_creds* remote_guard_creds_new_nocopy(char* package_name,
+        unsigned credenial_size,
+        BYTE*    credential);
+remote_guard_creds* remote_guard_creds_deepcopy(remote_guard_creds* that);
+void remote_guard_creds_add_supplemental_cred(remote_guard_creds* that,
+        remote_guard_package_cred* supplemental_cred);
+void remote_guard_creds_free(remote_guard_creds* that);
+
+auth_identity* auth_identity_new_password(SEC_WINNT_AUTH_IDENTITY* password_creds);
+auth_identity* auth_identity_new_smartcard(smartcard_creds* smartcard_creds);
+auth_identity* auth_identity_new_remote_guard(remote_guard_creds* remote_guard_creds);
+auth_identity* auth_identity_deepcopy(auth_identity* that);
+void auth_identity_free(auth_identity* that);
+const char* auth_identity_credential_type_label(auth_identity* that);
+
+size_t nla_sizeof_ts_creds(auth_identity* identity);
+size_t nla_write_ts_creds(auth_identity* identity, wStream* s);
+size_t nla_sizeof_ts_credentials_inner(auth_identity* identity);
+
+/* exported for tests */
+size_t nla_sizeof_ts_cspdatadetail_inner(csp_data_detail* csp_data);
+size_t nla_sizeof_ts_cspdatadetail(csp_data_detail*  csp_data);
+size_t nla_sizeof_ts_smartcard_creds_inner(smartcard_creds* smartcard_creds);
+size_t nla_sizeof_ts_smartcard_creds(smartcard_creds* smartcard_creds);
+size_t nla_sizeof_ts_remote_guard_package_cred_inner(remote_guard_package_cred* package_cred);
+size_t nla_sizeof_ts_remote_guard_package_cred(remote_guard_package_cred* package_cred);
+size_t nla_sizeof_ts_remote_guard_creds_inner(remote_guard_creds* remote_guard_creds);
+size_t nla_sizeof_ts_remote_guard_creds(remote_guard_creds* remote_guard_creds);
+
+size_t nla_write_ts_csp_data_detail(csp_data_detail* csp_data, int contextual_tag,  wStream* s);
+size_t nla_write_ts_smartcard_creds(smartcard_creds* smartcard_creds, wStream* s);
+size_t nla_write_ts_remote_guard_package_cred(remote_guard_package_cred* package_cred, wStream* s);
+size_t nla_write_ts_remote_guard_creds(remote_guard_creds*  remote_guard_creds, wStream* s);
+
 #define CHECK_MEMORY(pointer, result, description, ...)                 \
 	do                                                              \
 	{                                                               \
@@ -239,11 +312,7 @@ static SECURITY_STATUS nla_encrypt_ts_credentials(rdpNla* nla);
 static SECURITY_STATUS nla_decrypt_ts_credentials(rdpNla* nla);
 static BOOL nla_read_ts_password_creds(rdpNla* nla, wStream* s);
 
-#define ber_sizeof_sequence_octet_string(length) \
-	ber_sizeof_contextual_tag(ber_sizeof_octet_string(length)) + ber_sizeof_octet_string(length)
-#define ber_write_sequence_octet_string(stream, context, value, length)                \
-	ber_write_contextual_tag(stream, context, ber_sizeof_octet_string(length), TRUE) + \
-	    ber_write_octet_string(stream, value, length)
+
 
 /* CredSSP Client-To-Server Binding Hash\0 */
 static const BYTE ClientServerHashMagic[] = { 0x43, 0x72, 0x65, 0x64, 0x53, 0x53, 0x50, 0x20,
@@ -368,6 +437,18 @@ SEC_WINNT_AUTH_IDENTITY* SEC_WINNT_AUTH_IDENTITY_new(char* user,  char* password
 	return password_creds;
 }
 
+void SEC_WINNT_AUTH_IDENTITY_free(SEC_WINNT_AUTH_IDENTITY* password_creds)
+{
+	if (password_creds)
+	{
+		WSTRING_LENGTH_CLEAR_AND_FREE(password_creds, User);
+		WSTRING_LENGTH_CLEAR_AND_FREE(password_creds, Domain);
+		WSTRING_LENGTH_CLEAR_AND_FREE(password_creds, Password);
+	}
+
+	free(password_creds);
+}
+
 SEC_WINNT_AUTH_IDENTITY* SEC_WINNT_AUTH_IDENTITY_deepcopy(SEC_WINNT_AUTH_IDENTITY* original)
 {
 	SEC_WINNT_AUTH_IDENTITY* copy;
@@ -388,18 +469,6 @@ SEC_WINNT_AUTH_IDENTITY* SEC_WINNT_AUTH_IDENTITY_deepcopy(SEC_WINNT_AUTH_IDENTIT
 	}
 
 	return copy;
-}
-
-void SEC_WINNT_AUTH_IDENTITY_free(SEC_WINNT_AUTH_IDENTITY* password_creds)
-{
-	if (password_creds)
-	{
-		WSTRING_LENGTH_CLEAR_AND_FREE(password_creds, User);
-		WSTRING_LENGTH_CLEAR_AND_FREE(password_creds, Domain);
-		WSTRING_LENGTH_CLEAR_AND_FREE(password_creds, Password);
-	}
-
-	free(password_creds);
 }
 
 
@@ -605,6 +674,26 @@ void remote_guard_creds_add_supplemental_cred(remote_guard_creds* that,
 	that->supplemental_creds_count++;
 }
 
+void remote_guard_creds_free(remote_guard_creds* that)
+{
+	remote_guard_package_cred_free(that->login_cred);
+
+	if (that->supplemental_creds != NULL)
+	{
+		unsigned i;
+
+		for (i = 0; i < that->supplemental_creds_count; i++)
+		{
+			remote_guard_package_cred_free(that->supplemental_creds[i]);
+		}
+
+		memory_clear_and_free(that->supplemental_creds,
+		                      that->supplemental_creds_count * sizeof(that->supplemental_creds[0]));
+	}
+
+	memory_clear_and_free(that, sizeof(* that));
+}
+
 remote_guard_creds* remote_guard_creds_deepcopy(remote_guard_creds* that)
 {
 	unsigned i;
@@ -633,26 +722,6 @@ remote_guard_creds* remote_guard_creds_deepcopy(remote_guard_creds* that)
 	}
 
 	return copy;
-}
-
-void remote_guard_creds_free(remote_guard_creds* that)
-{
-	remote_guard_package_cred_free(that->login_cred);
-
-	if (that->supplemental_creds != NULL)
-	{
-		unsigned i;
-
-		for (i = 0; i < that->supplemental_creds_count; i++)
-		{
-			remote_guard_package_cred_free(that->supplemental_creds[i]);
-		}
-
-		memory_clear_and_free(that->supplemental_creds,
-		                      that->supplemental_creds_count * sizeof(that->supplemental_creds[0]));
-	}
-
-	memory_clear_and_free(that, sizeof(* that));
 }
 
 /* ============================================================ */
@@ -963,6 +1032,20 @@ size_t nla_sizeof_ts_remote_guard_creds_inner(remote_guard_creds* remote_guard_c
 size_t nla_sizeof_ts_remote_guard_creds(remote_guard_creds* remote_guard_creds)
 {
 	return ber_sizeof_sequence(nla_sizeof_ts_remote_guard_creds_inner(remote_guard_creds));
+}
+
+static size_t nla_sizeof_ts_password_creds(rdpNla* nla)
+{
+	size_t length = 0;
+
+	if (nla->identity->creds.password_creds)
+	{
+		length += ber_sizeof_sequence_octet_string(nla->identity->creds.password_creds->DomainLength * 2);
+		length += ber_sizeof_sequence_octet_string(nla->identity->creds.password_creds->UserLength * 2);
+		length += ber_sizeof_sequence_octet_string(nla->identity->creds.password_creds->PasswordLength * 2);
+	}
+
+	return length;
 }
 
 
@@ -2862,20 +2945,6 @@ fail:
 	free(buffer);
 	winpr_Digest_Free(sha256);
 	return status;
-}
-
-static size_t nla_sizeof_ts_password_creds(rdpNla* nla)
-{
-	size_t length = 0;
-
-	if (nla->identity->creds.password_creds)
-	{
-		length += ber_sizeof_sequence_octet_string(nla->identity->creds.password_creds->DomainLength * 2);
-		length += ber_sizeof_sequence_octet_string(nla->identity->creds.password_creds->UserLength * 2);
-		length += ber_sizeof_sequence_octet_string(nla->identity->creds.password_creds->PasswordLength * 2);
-	}
-
-	return length;
 }
 
 static size_t nla_sizeof_ts_credentials(rdpNla* nla)
