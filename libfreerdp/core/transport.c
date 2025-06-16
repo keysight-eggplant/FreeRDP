05e4fabdf26cc4de3e7ddb287642bc2040f8f0fa
Johnathan Becker <john.becker@keysight.com>
2025-06-16 11:10:05 -0600

    oops

--- libfreerdp/core/transport.c
+++ libfreerdp/core/transport.c
@@ -322,8 +322,8 @@ BOOL transport_connect_nla(rdpTransport *transport) {
 
   if (settings->AuthenticationServiceClass) {
     if (!nla_set_service_principal(
-            rdp->nla, nla_make_spn((char *)settings->AuthenticationServiceClass,
-                                   settings->ServerHostname)))
+            rdp->nla, (char *)nla_make_spn(settings->AuthenticationServiceClass,
+                                           settings->ServerHostname)))
       return FALSE;
   }
 
